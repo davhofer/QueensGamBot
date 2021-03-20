1b1e3d5b3aab6c21d186f761d7ecba09574fafa0
David <davhofer@ethz.ch>
2021-03-20 18:40:21 +0100

    latency function

--- main.py
+++ main.py
@@ -201,11 +201,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def latency(ctx):
     """Returns the latency of the bot"""
-    ms = (ctx.message.created_at - datetime.datetime.now()).milliseconds
-    edit = str(ctx.message.created_at - datetime.datetime.utcnow())
+    ms = (ctx.message.created_at - datetime.now()).milliseconds
+    edit = str(ctx.message.created_at - datetime.utcnow())
     answer = 'Latency is ' +str(int(float(edit.split(':')[2])*1000)) + 'ms'
     await ctx.send(answer)
-    await ctx.send(str(ms))
+    #await ctx.send(str(ms))
 
 
 
