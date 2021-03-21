0619804d2f4f40d8ed6f792331814897da99f3f8
david <davhofer@ethz.ch>
2021-03-21 16:39:09 +0100

    chess.com name mapping

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ async def chessdotcom(ctx, name: str):
 @bot.command(name="username")
 async def save_username(ctx, name: str):
     """save your chess.com username"""
-    name_mapping[ctx.message.author] = username
+    name_mapping[ctx.message.author] = name
     with open('data.json', 'w') as f:
          json.dump(data, f)
          f.close()
