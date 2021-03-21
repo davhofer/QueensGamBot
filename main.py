27b6929d339284048056be1c0ca794dd69a9cbe7
david <davhofer@ethz.ch>
2021-03-21 16:26:21 +0100

    chess.com name mapping

--- main.py
+++ main.py
@@ -197,8 +197,9 @@ async def chessdotcom(ctx, name: str):
             print(Exception)
             await ctx.send("There is no chess.com user with that username!")
 
-@chessdotcom.command(name="*name")
+@chessdotcom.command(name="name")
 async def _name(ctx, username: str):
+    """save your chess.com username"""
     name_mapping[ctx.message.author] = username
     with open('data.json', 'w') as f:
          json.dump(data, f)
